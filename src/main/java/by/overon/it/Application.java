ea4afc98a4b5fc012fe31285684379cccacc5785
aleksey <worcers@gmail.com>
2022-01-27 10:26:42 +0300

    fix problem with start application

--- src/main/java/by/overon/it/Application.java
+++ src/main/java/by/overon/it/Application.java
@@ -3,12 +3,14 @@ package by.overon.it;
 import by.overon.it.tg_bot.Bot;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
     @SneakyThrows
     public static void main(String[] args) {
