b5a262c0f06c2d70010c98705949c7a8fe1813ff
aleksey <worcers@gmail.com>
2022-01-28 10:21:45 +0300

    add enums

---
+++ src/main/java/by/overon/it/enums/BotStatusEnums.java
@@ -0,0 +1,12 @@
+package by.overon.it.enums;
+
+public enum BotStatusEnums {
+    ASK_ABOUT_BRAND,
+    ASK_ABOUT_MODEL,
+    ASK_ABOUT_MILEAGE,
+    ASK_ABOUT_YEAR,
+    ASK_ABOUT_DESCRIPTION,
+    ASK_ABOUT_PRICE,
+    ASK_ABOUT_PHOTO,
+    ASK_ABOUT_TELEPHONE
+}
