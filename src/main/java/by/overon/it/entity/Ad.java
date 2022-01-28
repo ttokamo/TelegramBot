d0ff438430a34179c23fa6bc88dcf14fbfc2c671
VorobeyPavel <Vorobey_Pavel@mail.ru>
2022-01-28 10:22:43 +0300

    new class BotStatus and refactor Ad class

--- src/main/java/by/overon/it/entity/Ad.java
+++ src/main/java/by/overon/it/entity/Ad.java
@@ -1,8 +1,7 @@
 package by.overon.it.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -10,21 +9,31 @@ import java.math.BigDecimal;
 @Entity
 @Table
 @NoArgsConstructor
-@Data
+
 public class Ad {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, nullable = false, unique=true)
     private String id;
+    @Setter
     private String user_id;
+    @Setter
     private String brand;
+    @Setter
     private String model;
+    @Setter
     private String mileage;
+    @Setter
     private String year;
+    @Setter
     private BigDecimal price;
+    @Setter
     private String text;
+    @Setter
     private byte[] photo;
+    @Setter
     private String telephone;
+    @Setter
     private String status;
 }
